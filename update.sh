e2dc64872c923b174ce855fb242c2c5dd5e4249f
onekb <1@1kb.ren>
2021-04-06 10:35:05 +0800

    update

--- update.sh
+++ update.sh
@@ -1,14 +1,14 @@
 export http_proxy=http://127.0.0.1:1087;export https_proxy=http://127.0.0.1:1087;
 git pull
-composer create-project --prefer-dist qcloud/discuz:v2.3.210308 -vvv --repository=https://cloud.discuz.chat discuzQ-github
-curl https://cloud.discuz.chat/dist/discuz/core/discuz-core-v2.3.210308-62419b.zip -o core.zip
+composer create-project --prefer-dist qcloud/discuz:v2.3.210311 -vvv --repository=https://cloud.discuz.chat discuzQ-github
+curl https://cloud.discuz.chat/dist/discuz/core/discuz-core-v2.3.210311-aefc79.zip -o core.zip
 unzip -o -d ./discuzQ-github/framework core.zip
 cd ./discuzQ-github
 composer install -vvv
 cp -R ../.git/. ./.git
 git add .
-git commit -m 'Discuz! Q RC v2.3.210308'
-# git commit -m 'Discuz! Q ALPHA v2.3.210208'
+# git commit -m 'Discuz! Q RC v2.3.210308'
+git commit -m 'Discuz! Q ALPHA v2.3.210311'
 git push -f
 cd ..
 rm -rf discuzQ-github core.zip
